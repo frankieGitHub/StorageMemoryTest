8fad64dfd6da87cb638bdc760ef94efe9ef4fdce
frankie <frankie_lrh@163.com>
2016-06-05 10:53:42 +0800

    add log

--- app/src/main/java/com/cunchu/yulong/storagememorytest/MainActivity.java
+++ app/src/main/java/com/cunchu/yulong/storagememorytest/MainActivity.java
@@ -12,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.util.*;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -19,6 +20,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+		Log.d("aaa","bbb");
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
